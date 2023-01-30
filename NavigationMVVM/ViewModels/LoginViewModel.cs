53ea6842a77749c16d3082f8a6f0b97654990fac
Bumindu Yasith <bumiduyasith@gmail.com>
2023-01-30 12:57:10 +0530

    create login view & loginviewmodel

---
+++ NavigationMVVM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NavigationMVVM.Stores;
+using NavigationMVVM.Views;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NavigationMVVM.ViewModels
+{
+    public partial class LoginViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        public string username;
+
+        [ObservableProperty]
+        public string password;
+
+        private readonly NavigationStore navigationStore;
+
+
+        public LoginViewModel()
+        {
+            navigationStore = Locator.Current.GetService<NavigationStore>();
+        }
+
+        [RelayCommand]
+        public void Login()
+        {
+            
+        }
+    }
+
+}
---
+++ NavigationMVVM/Views/UserControls/LoginView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace NavigationMVVM.Views.UserControls
+{
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
