4c42b60b0010446b0a5de29dcae5ebe5739323f1
Bumindu Yasith <bumiduyasith@gmail.com>
2023-01-30 13:01:00 +0530

    get navigation service and initilize the viewmodel

--- NavigationMVVM/ViewModels/MainWindowViewModel.cs
+++ NavigationMVVM/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,32 @@
 ﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using NavigationMVVM.Stores;
+using NavigationMVVM.Views;
+using Splat;
+
 namespace NavigationMVVM.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+        private readonly NavigationStore _navigationStore;
+
+        [ObservableProperty]
+        public ViewModelBase currentViewModel;
+
+        public MainWindowViewModel()
+        {
+            _navigationStore = Locator.Current.GetService<NavigationStore>()!;
+
+            CurrentViewModel = _navigationStore.CurrentViewModel;
+
+            _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
     }
 }
