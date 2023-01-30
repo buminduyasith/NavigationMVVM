c9123613622b6db35c9c6e5b197163c03225fc6a
Bumindu Yasith <bumiduyasith@gmail.com>
2023-01-30 11:50:05 +0530

    Add project files.

---
+++ NavigationMVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace NavigationMVVM.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ NavigationMVVM/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace NavigationMVVM.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
